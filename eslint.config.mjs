67c1b489aee778b09ebb208d084e4a3602767416
Дарья Филиппова <darfilp@MacBook-Air-Dara.local>
2025-04-30 13:16:26 +0300

    lesson 2. lesson 19. add stylelint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
+import i18next from "eslint-plugin-i18next";
 
 export default defineConfig([
   {
@@ -30,4 +31,5 @@ export default defineConfig([
       },
     },
   },
+  i18next.configs["flat/recommended"],
 ]);
--- src/widgets/themeToggle/ui/ThemeToggle.tsx
+++ src/widgets/themeToggle/ui/ThemeToggle.tsx
@@ -1,6 +1,5 @@
 import clsx from "clsx";
 import { useTheme } from "shared/theme/useTheme";
-import s from "./ThemeToggle.m.scss";
 import DarkMode from "shared/assets/icons/dark_mode.svg";
 import LightMode from "shared/assets/icons/light_mode.svg";
 import { Button } from "shared/lib/Button";
@@ -14,7 +13,7 @@ export const ThemeToggle = ({ className }: ThemeToggleProps) => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <Button className={clsx(s.root, className)} onClick={toggleTheme}>
+    <Button className={clsx(className)} onClick={toggleTheme}>
       {theme === Theme.LIGHT ? (
         <DarkMode fill="white" />
       ) : (
